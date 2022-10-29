c60131694824bf2cea44139570738825c862dae6
Mariana Cristina Dias dos Santos <marianacristinasantos05@gmail.com>
2022-10-29 17:13:38 -0300

    modify app.js file

--- exercicios/r-library/src/app.js
+++ exercicios/r-library/src/app.js
@@ -1,13 +1,15 @@
 const express = require("express"); //importo o express
+const cors = require("cors");
+const mongoose = require("./database/mongooseConnect");
+const routes = require("./routes/booksRoute");
 
 const app = express(); // executo o express
 
 app.use(express.json()); // uso o body parse
+app.use(cors())
+mongoose.connect()
 
-// importe da continuação de rotas podcast
-const booksRoute = require("./routes/booksRoute");
-
-app.use("/library/books", booksRoute); // crio uma rota raiz
+app.use("/library/books", routes); // crio uma rota raiz
 
 // exportando para usar o server.js
 module.exports = app;

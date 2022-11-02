891c36ce5949466276a478ed19d97092f552e66e
pâmela santos <pssantos201@gmail.com>
2022-11-02 13:44:03 -0300

    atualiza repo

--- exercicios/r-library/src/database/mongooseConnect.js
+++ exercicios/r-library/src/database/mongooseConnect.js
@@ -2,10 +2,13 @@ const mongoose = require("mongoose");
 
 const connect = async () => {
   try {
-    mongoose.connect("mongodb+srv://GaiaMaria8:gaia1999@cluster0.btpmh.mongodb.net/rlibrary", {
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    });
+    mongoose.connect(
+      "mongodb+srv://tipopamela:Alcoolgel20@cluster0.btpmh.mongodb.net/rlibrary",
+      {
+        useNewUrlParser: true,
+        useUnifiedTopology: true,
+      }
+    );
     console.log("Database connected");
   } catch (error) {
     console.log(error);
@@ -15,4 +18,3 @@ const connect = async () => {
 module.exports = {
   connect,
 };
-
--- exercicios/r-library/src/models/booksModel.js
+++ exercicios/r-library/src/models/booksModel.js
@@ -1,40 +1,38 @@
 const mongoose = require("mongoose");
 
-const BookSchema = new mongoose.Schema(
-  {
-    _id: {
-      type: mongoose.Schema.Types.ObjectId,
-      default: mongoose.Types.ObjectId,
-    },
-    title: {
-      type: String,
-      required: true,
-      unique: true,
-    },
-    launchYear: {
-      type: Number,
-      required: true,
-    },
-    available: {
-      type: Boolean,
-      required: true,
-    },
-    publisher: {
-      type: String,
-      required: true,
-    },
-    gender: {
-      type: String,
-      required: true,
-    },
-    writer: {
-      type: String,
-      required: true,
-    },
-    pages: Number,
+const BookSchema = new mongoose.Schema({
+  _id: {
+    type: mongoose.Schema.Types.ObjectId,
+    default: mongoose.Types.ObjectId,
   },
-);
+  title: {
+    type: String,
+    require: true,
+    unique: true,
+  },
+  launchYear: {
+    type: Number,
+    require: true,
+  },
+  available: {
+    type: Boolean,
+    require: true,
+  },
+  publisher: {
+    type: String,
+    require: true,
+  },
+  genre: {
+    type: String,
+    require: true,
+  },
+  author: {
+    type: String,
+    require: true,
+  },
+  pages: Number,
+});
 
-const Model = mongoose.model("Book", BookSchema);
+const Model = mongoose.model("book", BookSchema);
 
 module.exports = Model;

0f3f7292441a926106be77161846f2f48d3ac506
Dri Costa <adrianeabc_ap@hotmail.com>
2022-11-03 00:51:33 -0300

    Resumo da aula

--- exercicios/r-library/src/routes/booksRoute.js
+++ exercicios/r-library/src/routes/booksRoute.js
@@ -1,3 +1,4 @@
+//rotas
 const controller = require("../controllers/booksController");
 
 const express = require("express");
@@ -8,9 +9,9 @@ const router = express.Router();
 // router. metodo http (rota, funcao)
 
 router.get("/all", controller.getAllBooks);
-router.get("/:id", controller.getBooksById);
+router.get("/:id", controller.getBookById);
 router.post("/new", controller.addNewBook);
-router.put("/:id", controller.updateBooks);
-router.delete("/:id", controller.deleteBooks);
+router.patch("/:id", controller.updateBookById);
+router.delete("/:id", controller.deleteBook);
 
 module.exports = router;

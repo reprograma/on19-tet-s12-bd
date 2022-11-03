00bd1b2872024abde0babf980447d0dbb94db241
Valéria Cabral <valeriacabral1969@gmail.com>
2022-11-03 15:32:29 -0300

    atualização com meus dados do mongoo

--- exercicios/r-library/src/database/mongooseConnect.js
+++ exercicios/r-library/src/database/mongooseConnect.js
@@ -2,7 +2,7 @@ const mongoose = require("mongoose");
 
 const connect = async () => {
   try {
-    mongoose.connect("mongodb+srv://GaiaMaria8:gaia1999@cluster0.btpmh.mongodb.net/rlibrary", {
+      mongoose.connect("mongodb+srv://ValeriaCabral:161071@cluster0.jtdu9bi.mongodb.net/rlibrary", {
       useNewUrlParser: true,
       useUnifiedTopology: true,
     });
